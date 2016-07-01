1f5f0beaa6c5c4d4fb8d10a3eef6c0cceb9bf177
Max Chadwick <mpchadwick@gmail.com>
2016-07-01 03:01:06 -0400

    Tracker userAgent, isBot

--- app/code/community/Mpchadwick/PageCacheHitRate/Model/Tracker/ParamProvider.php
+++ app/code/community/Mpchadwick/PageCacheHitRate/Model/Tracker/ParamProvider.php
@@ -2,6 +2,25 @@
 
 class Mpchadwick_PageCacheHitRate_Model_Tracker_ParamProvider
 {
+    // Taken from Cm_RedisSession_Model_Session
+    const BOT_REGEX = '/^alexa|^blitz\.io|bot|^browsermob|crawl|^curl|^facebookexternalhit|feed|google web preview|^ia_archiver|^java|jakarta|^load impact|^magespeedtest|monitor|nagios|^pinterest|postrank|slurp|spider|uptime|yandex/i';
+
+    protected $userAgent;
+
+    protected $isBot;
+
+    public function __construct()
+    {
+        $this->userAgent = empty($_SERVER['HTTP_USER_AGENT']) ? '' : $_SERVER['HTTP_USER_AGENT'];
+        if ($this->userAgent) {
+            $this->isBot = preg_match(self::BOT_REGEX, $this->userAgent);
+        } else {
+            // Can't be sure, but let's say it's not a bot...
+            $this->isBot = 0;
+        }
+
+    }
+
     public function baseParams($originalRequest = false)
     {
         return array(
@@ -9,6 +28,8 @@ class Mpchadwick_PageCacheHitRate_Model_Tracker_ParamProvider
             'ip' => Mage::app()->getRequest()->getClientIp(),
             'hostname' => gethostname(),
             'customerGroup' => $this->getCustomerGroup(),
+            'userAgent' => $this->userAgent,
+            'isBot' => $this->isBot,
         );
     }
 
